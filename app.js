07b91002cd87cdd0aca2e04f8fb3ee0dff4fc90a
joel.j <joel.j@kakaocorp.com>
2026-06-11 20:20:27 +0900

    Update wedding invitation layout and gallery

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const galleryImages = Array.from({ length: 9 }, (_, index) => {
   const versions = {
     "03": "?v=20260609-2",
     "05": "?v=20260609-2",
-    "07": "?v=20260609-2",
+    "07": "?v=20260611-1",
     "08": "?v=20260609-3"
   };
   const version = versions[number] || "";
